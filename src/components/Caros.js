d70873d52caf25bffe141438f9c452c2d9771594
hassaine <hassaine24@gmail.com>
2020-12-24 21:35:20 +0100

    add Testimonial-beta section

--- src/components/Caros.js
+++ src/components/Caros.js
@@ -1,32 +1,30 @@
-import template from "../assets/img/blog-4.jpg";
-import {Link } from "react-router-dom"
-function Caros(){
-    return (
-
-      <div class="carousel">
-          <div class="container-fluid">
-              <div class="owl-carousel">
-                  <div class="carousel-item">
-                      <div class="carousel-img">
-                          <img  src={template} alt="temp" />
-                      </div>
-                      <div class="carousel-text">
-                          <h1>IGETIS Web</h1>
-                          <p>
-                            Création de site internet dynamique et référencement
-                          </p>
-                          <div class="carousel-btn">
-                              <Link to="/Contact"><a class="btn" href=""><i class="fa fa-email"></i>Nous contacter</a></Link>
-                          </div>
-                      </div>
-                  </div>
+import template from '../assets/img/meeting.jpg';
+import { Link } from 'react-router-dom';
+function Caros() {
+  return (
+    <div class="carousel">
+      <div class="container-fluid">
+        <div class="owl-carousel">
+          <div class="carousel-item">
+            <div class="carousel-img">
+              <img src={template} alt="temp" />
+            </div>
+            <div class="carousel-text">
+              <h1>IGETIS Web</h1>
+              <p>Création de site internet dynamique et référencement</p>
+              <div class="carousel-btn">
+                <Link to="/Contact">
+                  <a class="btn" href="">
+                    <i class="fa fa-email"></i>Nous contacter
+                  </a>
+                </Link>
               </div>
+            </div>
           </div>
+        </div>
       </div>
+    </div>
+  );
+}
 
-    );
-  }
-
-
-
-export default Caros
+export default Caros;
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
 import '../styles/css/owl.css';
+import img1 from '../assets/img/testimonial-1.jpg';
 // import './owl.css';
 {
   /* <img src="img/testimonial-1.jpg" alt="Image" />; */
 }
 const Testimonial = () => {
   return (
-    <div>
+    <div class="testimonial">
       <div class="container-fluid">
         <div className="row title" style={{ marginBottom: '20px' }}>
           <div class="col-sm-12 btn btn-info">
@@ -18,34 +19,91 @@ const Testimonial = () => {
         </div>
       </div>
 
-      <div class="container-fluid">
-        <OwlCarousel items={4} margin={8} autoplay={true}>
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+      <div class="container">
+        <OwlCarousel
+          class="testimonials-carousel owl-carousel"
+          items={3}
+          // margin={3}
+          animateOut={'fadeOut'}
+          animateIn={'fadeIn'}
+          smartSpeed={300}
+          //     animateIn: 'fadeIn',
+          dots={false}
+          autoplay={true}
+          nav={true}
+          navText={[
+            "<i class='fa fa-angle-left' aria-hidden='true'></i>",
+            "<i class='fa fa-angle-right' aria-hidden='true'></i>",
+          ]}
+        >
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
 
-          <div>
-            <img src="img/testimonial-1.jpg" alt="Image" />
+          <div class="testimonial-item">
+            <img src={img1} alt="Image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              ut mollis mauris. Vivamus egestas eleifend dui ac consequat
+            </p>
+            <h2>Client Name</h2>
+            <h3>Profession</h3>
           </div>
         </OwlCarousel>
       </div>
