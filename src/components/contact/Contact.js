f03e8571088a050a25fa3c609f400f9b5afebaeb
Amarni Faycal <faycalamarni@gmail.com>
2020-12-24 21:45:26 +0100

    m

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ContactHome from './ContactHome'
 import Background from "../../assets/img/contact.jpg"
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const Contact = (props) => {
   return (
-    <div className="contact">
+    <div className="contact container">
         <section className="hero-wrap hero-wrap-2" style={{backgroundImage: `url(${Background})`}} data-stellar-background-ratio="0.5">
           <div className="overlay"></div>
           <div className="container">
@@ -18,6 +21,17 @@ const Contact = (props) => {
         <div class="mt-125">
             <ContactHome />
         </div>
+
+        <OwlCarousel>
+            <div class="item"><h4>1</h4></div>
+            <div class="item"><h4>2</h4></div>
+            <div class="item"><h4>3</h4></div>
+            <div class="item"><h4>4</h4></div>
+            <div class="item"><h4>5</h4></div>
+            <div class="item"><h4>6</h4></div>
+        </OwlCarousel>
+
+
     </div>
   )
 }
