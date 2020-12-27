d1632234344df0774c653a26592337256e15729f
hassaine <hassaine24@gmail.com>
2020-12-27 04:29:06 +0100

    start referencement section

--- src/components/referencement/Referencement.js
+++ src/components/referencement/Referencement.js
@@ -1,10 +1,61 @@
 import React from 'react';
-
+import latest from '../../assets/img/latest-1.jpg';
+import '../../styles/css/referencement.css';
 const Referencement = () => {
   return (
-    <div>
-      <h1>Referencement</h1>
-    </div>
+    <section class="latest-blog spad">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="section-title text-center">
+              <h1>Référencement</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src={latest} alt="Avatar" />
+                </div>
+                <div class="flip-card-back">
+                  <h1>John Doe</h1>
+                  <p>Architect & Engineer</p>
+                  <p>We love that guy</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src={latest} alt="Avatar" />
+                </div>
+                <div class="flip-card-back">
+                  <h1>John Doe</h1>
+                  <p>Architect & Engineer</p>
+                  <p>We love that guy</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src={latest} alt="Avatar" />
+                </div>
+                <div class="flip-card-back">
+                  <img src={latest} alt="Avatar" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
